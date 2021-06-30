1e8d90c71590f01275b658c41c462e35f08e0b14
Mahmuod1 <Mahmuod1@users.noreply.github.com>
2021-06-30 18:59:51 +0000

    automated commit by submit50

--- cash.c
+++ cash.c
@@ -5,38 +5,38 @@
 
 int main(void)
 {
-    int coins=0;
-        float change;
-        do
-        {
-            change = get_float("Change Owe :");
+    int coins = 0;
+    float change;
+    do
+    {
+        change = get_float("Change Owe :");
 
-        }
-        while(change<=0);
-        int toCents = round(change *100);
-        while(toCents-25>=0)
-        {
-            toCents =toCents-25;
-            coins++;
+    }
+    while (chang <= 0);
+    int toCents = round(change * 100);
+    while (toCents - 25 >= 0)
+    {
+        toCents = toCents - 25;
+        coins++;
 
-        }
-        while(toCents-10>=0)
-        {
-            toCents = toCents - 10;
-            coins++;
-        }
-        while (toCents - 5 >= 0)
-        {
-            toCents = toCents -5;
-            coins++;
-        }
+    }
+    while (toCents - 10 >= 0)
+    {
+        toCents = toCents - 10;
+        coins++;
+    }
+    while (toCents - 5 >= 0)
+    {
+        toCents = toCents - 5;
+        coins++;
+    }
         
-        while (toCents -1 >= 0)
-        {
-            toCents = toCents -1;
-            coins++;
-        }
-        printf("%i\n",coins);
+    while (toCents - 1 >= 0)
+    {
+        toCents = toCents - 1;
+        coins++;
+    }
+    printf("%i\n", coins);
       
 
 }
