e64f9c4d33b3eaa59ec62f1007a854848c48f7bf
Mahmuod1 <Mahmuod1@users.noreply.github.com>
2021-06-30 17:18:50 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -1,26 +1,27 @@
 #include <cs50.h>
 #include <stdio.h>
 void getAlignetBlocks(int height);
-int main (void)
+int main(void)
 {
     int height = 0;
-    do{
+    do
+    {
         height = get_int("height : ");
     }
-    while(height<=0||height>8);
-        getAlignetBlocks(height);
+    while (height <= 0 || height > 8);
+    getAlignetBlocks(height);
 }
 
 void getAlignetBlocks(int height)
 {
 
-     for(int i=1;i<=height;i++)
+    for (int i = 1; i <= height; i++)
     {
-        for(int j=height;j>i;j--)
+        for (int j = height; j > i; j--)
         {
             printf(" ");
         }
-        for(int k=i-i;k<i;k++)
+        for (int k = i - i; k < i; k++)
         {
             printf("#");
         }
