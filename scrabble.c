0ad7a8572763f0b2f762a74455eacc62184d8695
Mahmuod1 <Mahmuod1@users.noreply.github.com>
2021-07-02 11:18:14 +0000

    automated commit by check50 [check50=True]

--- scrabble.c
+++ scrabble.c
@@ -17,7 +17,14 @@ int main(void)
     // Score both words
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
-
+if(score1<score2){
+    printf("Player 2 wins!\n");
+}else if (score1>score2)
+{
+    printf("Player 1 wins!\n");
+}else{
+    printf("draw");
+}
     // TODO: Print the winner
 }
 
