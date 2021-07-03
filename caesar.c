53b3ecc6e5e9099a77b2f17f3aa3782330744a33
Mahmuod1 <Mahmuod1@users.noreply.github.com>
2021-07-03 11:01:53 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -4,48 +4,52 @@
 #include  <ctype.h>
 #include <stdlib.h>
 
-int main(int argc,string argv[])
+int main(int argc, string argv[])
 {
 
 
     int key ;
     string keyString;
-if(argc==2){
-     keyString= argv[1];
-int keylen= strlen(keyString);
+    if (argc == 2)
+    {
+        keyString = argv[1];
+        int keylen = strlen(keyString);
 
-for (int i = 0 ; i < keylen; i++)
-{
-    char num = keyString[i];
- if(!isdigit(num)||atoi(keyString)<=0||argc!=2){
-    printf("Usage : ./caesar key\n");
-    return 1;
- }
-}
+        for (int i = 0 ; i < keylen; i++)
+        {
+            char num = keyString[i];
+            if (!isdigit(num) || atoi(keyString) <= 0 || argc != 2)
+            {
+                printf("Usage : ./caesar key\n");
+                return 1;
+            }
+        }
 
-}else{
-    return 1;
-}
-key = atoi(keyString);
- string text=get_string("plaintext: ");
+    }
+    else
+    {
+        return 1;
+    }
+    key = atoi(keyString);
+    string text = get_string("plaintext: ");
 
- const int lengt=strlen(text);
- for (int i =0; i<lengt ; i++)
- {
-     if(isupper(text[i]))
-     {
-      int p=text[i]-'A';
-    text[i]=((p+key)%26)+'A';
+    const int lengt = strlen(text);
+    for (int i = 0; i < lengt ; i++)
+    {
+        if (isupper(text[i]))
+        {
+            int p = text[i] - 'A';
+            text[i] = ((p + key) % 26) + 'A';
 
-     }
-     else if(islower(text[i]))
-     {
-  int p=text[i]-'a';
-    text[i]=((p+key)%26)+'a';
-     }
+        }
+        else if (islower(text[i]))
+        {
+            int p = text[i] - 'a';
+            text[i] = ((p + key) % 26) + 'a';
+        }
 
 
- }
-  printf("ciphertext: %s\n",text);
-  return 0;
+    }
+    printf("ciphertext: %s\n", text);
+    return 0;
 }
