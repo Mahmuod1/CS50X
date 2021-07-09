1df0c68f592b6f80940b2a6c0735edadda1e1f43
Mahmuod1 <Mahmuod1@users.noreply.github.com>
2021-07-09 15:01:28 +0000

    automated commit by submit50

--- helpers.c
+++ helpers.c
@@ -1,31 +1,17 @@
 #include "helpers.h"
 #include <math.h>
-// Convert image to grayscale
-
-// int upperLeft(int i ,int j)
-// {
-//     return image[i-1][j-1];
-// }
-// int upperRight(int i , int j)
-// {
-//     return image[i-1][j+1];
-// }
-// int downRight(int i ,int j)
-// {
-//     return image[i+1][j+1];
-// }
 
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-       for (int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            int average=round(( image[i][j].rgbtGreen +  image[i][j].rgbtBlue + image[i][j].rgbtRed )/3.0);
+            int average  = round((image[i][j].rgbtGreen +  image[i][j].rgbtBlue + image[i][j].rgbtRed) / 3.0);
 
-            image[i][j].rgbtGreen=average;
-            image[i][j].rgbtBlue=average;
-            image[i][j].rgbtRed=average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtBlue = average;
+            image[i][j].rgbtRed = average;
 
         }
     }
@@ -40,24 +26,24 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-         int spiaRed=round((.393 *  image[i][j].rgbtRed) +(.769  *  image[i][j].rgbtGreen) + (.189 *   image[i][j].rgbtBlue));
-         int spiaGreen=round((.349 *  image[i][j].rgbtRed) +(.686  *  image[i][j].rgbtGreen) + (.168 *   image[i][j].rgbtBlue));
-         int spiaBlue=round((.272 *  image[i][j].rgbtRed) +(.534  *  image[i][j].rgbtGreen) + (.131 *   image[i][j].rgbtBlue));
-         if(spiaRed>255)
-         {
-             spiaRed=255;
-         }
-         if(spiaGreen>255)
-         {
-             spiaGreen=255;
-         }
-         if(spiaBlue>255)
-         {
-             spiaBlue=255;
-         }
-            image[i][j].rgbtGreen=spiaGreen;
-            image[i][j].rgbtBlue=spiaBlue;
-            image[i][j].rgbtRed=spiaRed;
+            int spiaRed = round((.393 *  image[i][j].rgbtRed) + (.769  *  image[i][j].rgbtGreen) + (.189 *   image[i][j].rgbtBlue));
+            int spiaGreen = round((.349 *  image[i][j].rgbtRed) + (.686  *  image[i][j].rgbtGreen) + (.168 *   image[i][j].rgbtBlue));
+            int spiaBlue = round((.272 *  image[i][j].rgbtRed) + (.534  *  image[i][j].rgbtGreen) + (.131 *   image[i][j].rgbtBlue));
+            if (spiaRed > 255)
+            {
+                spiaRed = 255;
+            }
+            if (spiaGreen > 255)
+            {
+                spiaGreen = 255;
+            }
+            if (spiaBlue > 255)
+            {
+                spiaBlue = 255;
+            }
+            image[i][j].rgbtGreen = spiaGreen;
+            image[i][j].rgbtBlue = spiaBlue;
+            image[i][j].rgbtRed = spiaRed;
 
         }
     }
@@ -67,20 +53,21 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    for(int r = 0; r < height; r++){
-     for (int right=width-1,left=0 ;right>left;left++,right--)
-     {
-         int temp= image[r][left].rgbtGreen;
+    for (int r = 0; r < height; r++)
+    {
+        for (int right = width - 1, left = 0 ; right > left; left++, right--)
+        {
+            int temp = image[r][left].rgbtGreen;
          
-         image[r][left].rgbtGreen=image[r][right].rgbtGreen;
-      image[r][right].rgbtGreen=temp;
-      temp= image[r][left].rgbtBlue;
-          image[r][left].rgbtBlue=image[r][right].rgbtBlue;
-          image[r][right].rgbtBlue=temp;
-          temp=image[r][left].rgbtRed;
-           image[r][left].rgbtRed=image[r][right].rgbtRed;
-           image[r][right].rgbtRed=temp;
-     }
+            image[r][left].rgbtGreen = image[r][right].rgbtGreen;
+            image[r][right].rgbtGreen = temp;
+            temp = image[r][left].rgbtBlue;
+            image[r][left].rgbtBlue = image[r][right].rgbtBlue;
+            image[r][right].rgbtBlue = temp;
+            temp = image[r][left].rgbtRed;
+            image[r][left].rgbtRed = image[r][right].rgbtRed;
+            image[r][right].rgbtRed = temp;
+        }
 
         // Reset left and right after processing a row.
      
@@ -92,14 +79,14 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
     RGBTRIPLE temp [height][width];
-     for (int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
             int count = 0;
-            float totalR=0,totalG=0,totalB=0;
-            int rowCoords[] = {i-1,i,i+1};
-            int colCoords[] = {j-1,j,j+1};
+            float totalR = 0, totalG = 0, totalB = 0;
+            int rowCoords[] = {i - 1, i, i + 1};
+            int colCoords[] = {j - 1, j, j + 1};
             
             for (int r = 0; r < 3; r++)
             {
@@ -109,9 +96,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                     int currRow = rowCoords[r];
                     int currCol = colCoords[c];
                     
-                    if(currRow>=0&&currCol>=0&&currRow<height&&currCol<width)
+                    if (currRow >= 0 && currCol >= 0 && currRow < height && currCol < width)
                     {
-                        RGBTRIPLE pixle= image[currRow][currCol];
+                        RGBTRIPLE pixle = image[currRow][currCol];
                         totalR += pixle.rgbtRed;
                         totalB += pixle.rgbtBlue;
                         totalG += pixle.rgbtGreen;
@@ -119,17 +106,17 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                     }
                 }
             }
-                 temp[i][j].rgbtGreen = round(totalG/count);
-                 temp[i][j].rgbtBlue = round(totalB/count);
-                 temp[i][j].rgbtRed = round(totalR/count);
+            temp[i][j].rgbtGreen = round(totalG / count);
+            temp[i][j].rgbtBlue = round(totalB / count);
+            temp[i][j].rgbtRed = round(totalR / count);
          
         }
     }
-    for(int i = 0; i<height;i++)
+    for (int i = 0;  i < height; i++)
     {
-        for (int j = 0;j<width;j++)
+        for (int j = 0; j < width; j++)
         {
-            image[i][j]=temp[i][j];
+            image[i][j] = temp[i][j];
         }
     }
     return;
